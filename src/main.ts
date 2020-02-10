37b50d661552dd8dc33bd69b75efb3b45393c709
Karim <karim_daghari@outlook.com>
2020-02-10 19:13:34 +0100

    chore: security

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { constants } from './config/constants';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap(): Promise<void> {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { cors: true });
 	app.useGlobalPipes(new ValidationPipe());
-	app.enableCors();
-	app.use(helmet);
+	app.use(helmet());
+	app.use(csurf());
+	app.use(
+		rateLimit({
+			windowMs: 15 * 60 * 1000, // 15 minutes
+			max: 100 // limit each IP to 100 requests per windowMs
+		})
+	);
 	await app.listen(constants.port);
 }
 bootstrap();
