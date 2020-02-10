fdc56c549672a08a641140a6a5db3dbe6d0fcf1d
Karim <karim_daghari@outlook.com>
2020-02-10 18:03:00 +0100

    chore: add helmet

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { constants } from './config/constants';
+import * as helmet from 'helmet';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.enableCors();
+	app.use(helmet);
 	await app.listen(constants.port);
 }
 bootstrap();
