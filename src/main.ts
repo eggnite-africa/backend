bb59085e51f14186efe466ee580505335ed899de
Karim <karim_daghari@outlook.com>
2020-02-10 19:47:50 +0100

    chore: rm unsued packages

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { constants } from './config/constants';
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
 import * as rateLimit from 'express-rate-limit';
-import { Logger } from 'nestjs-pino';
 
 async function bootstrap(): Promise<void> {
-	const app = await NestFactory.create(AppModule, {
-		cors: true,
-		logger: false
-	});
+	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors();
 	app.use(helmet());
-	app.use(csurf());
 	app.use(
 		rateLimit({
 			windowMs: 15 * 60 * 1000, // 15 minutes
 			max: 100 // limit each IP to 100 requests per windowMs
 		})
 	);
-	app.useLogger(app.get(Logger));
 	await app.listen(constants.port);
 }
 bootstrap();
