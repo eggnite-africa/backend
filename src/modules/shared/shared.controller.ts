82fc4a9f7198c62c016eee42ca1b973da955a4eb
Karim <karim_daghari@outlook.com>
2020-02-14 23:28:05 +0100

    refactor: use service instead of defining everything in the controller

--- src/modules/shared/shared.controller.ts
+++ src/modules/shared/shared.controller.ts
@@ -1,33 +1,11 @@
-require('dotenv').config();
-import { Controller, Post, Param } from '@nestjs/common';
-import { S3 } from 'aws-sdk';
-import nanoid = require('nanoid');
+import { Controller, Post, Body } from '@nestjs/common';
+import { SharedService } from './shared.service';
 
 @Controller()
 export class SharedController {
+	constructor(private readonly sharedService: SharedService) {}
 	@Post('sign-s3')
-	signS3(
-		@Param('fileName') fileName: string,
-		@Param('fileType') fileType: string
-	): any {
-		const s3 = new S3();
-		const Key = nanoid();
-		const s3Params = {
-			Bucket: process.env.S3_BUCKET,
-			Key,
-			ContentType: fileType,
-			Expires: 60,
-			ACL: 'public-read',
-			Region: process.env.S3_REGION
-		};
-		s3.getSignedUrl('putObject', s3Params, (err, signedRequest) => {
-			if (err) {
-				throw Error('there was a problem signing the url');
-			}
-			return {
-				signedRequest,
-				url: `https://${s3Params.Bucket}.s3.amazonaws.com/${s3Params.Key}`
-			};
-		});
+	signS3(@Body('fileType') fileType: string): any {
+		return this.sharedService.signS3(fileType);
 	}
 }
