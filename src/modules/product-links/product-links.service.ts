d4fb529e831424d92596e144e678a332be32a5b4
Karim <karim_daghari@outlook.com>
2020-02-15 14:21:09 +0100

    fix: don't fetch entity, execute a delete op

--- src/modules/product-links/product-links.service.ts
+++ src/modules/product-links/product-links.service.ts
@@ -35,9 +35,8 @@ export class ProductLinksService {
 	}
 
 	async deleteLinks(id: number): Promise<boolean> {
-		const linksEntity = await this.productLinksRepository.findOneOrFail(id);
 		try {
-			await this.productLinksRepository.remove(linksEntity);
+			await this.productLinksRepository.delete(id);
 			return true;
 		} catch (e) {
 			throw new InternalServerErrorException(
