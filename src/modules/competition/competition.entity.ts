3a0606548209a6cb53e184c22d3ee8810c399994
Karim <karim_daghari@outlook.com>
2020-02-26 17:59:30 +0100

    chore: define first draft of entity

--- src/modules/competition/competition.entity.ts
+++ src/modules/competition/competition.entity.ts
@@ -2,9 +2,18 @@ import {
 	BaseEntity,
 	PrimaryGeneratedColumn,
 	CreateDateColumn,
-	UpdateDateColumn
+	UpdateDateColumn,
+	Column,
+	Entity,
+	ManyToOne
 } from 'typeorm';
+import { Field, ObjectType } from 'type-graphql';
+import { Product } from '../product/product.entity';
+import { IsNotEmpty } from 'class-validator';
+import { User } from '../user/user.entity';
 
+@ObjectType()
+@Entity()
 export class Competition extends BaseEntity {
 	@PrimaryGeneratedColumn()
 	readonly id!: number;
@@ -14,4 +23,23 @@ export class Competition extends BaseEntity {
 
 	@UpdateDateColumn()
 	updatedAt!: Date;
+
+	@Field(() => String)
+	@IsNotEmpty()
+	@Column({ unique: true })
+	name!: string;
+
+	@Field(() => String)
+	@IsNotEmpty()
+	@Column()
+	description!: string;
+
+	@Field(() => [Product])
+	products!: Product[];
+
+	@Field(() => [User])
+	moderators!: User[];
+
+	@Field(() => [User])
+	jury!: User[];
 }
