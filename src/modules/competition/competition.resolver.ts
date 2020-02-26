1680ccbaf49accfd090bea408a1e3daa68d592af
Karim <karim_daghari@outlook.com>
2020-02-26 18:52:48 +0100

    add query resolvers

--- src/modules/competition/competition.resolver.ts
+++ src/modules/competition/competition.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Competition } from './competition.entity';
+import { CompetitionService } from './competition.service';
+import { ID } from 'type-graphql';
 
-@Resolver('Competition')
-export class CompetitionResolver {}
+@Resolver(() => Competition)
+export class CompetitionResolver {
+	constructor(private readonly competitionService: CompetitionService) {}
+
+	@Query(() => [Competition])
+	async competitions(): Promise<Competition[]> {
+		return await this.competitionService.fetchAllCompetitions();
+	}
+
+	@Query(() => Competition)
+	async competition(
+		@Args({ name: 'id', type: () => ID, nullable: true }) id?: number,
+		@Args({ name: 'name', type: () => String, nullable: true }) name?: string
+	): Promise<Competition> {
+		return await this.competitionService.fetchCompetitionByIdOrName(id, name);
+	}
+}
--- src/modules/competition/competition.service.ts
+++ src/modules/competition/competition.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Competition } from './competition.entity';
 
 @Injectable()
-export class CompetitionService {}
+export class CompetitionService {
+	constructor(
+		private readonly competitionRepository: Repository<Competition>
+	) {}
+
+	async fetchAllCompetitions(): Promise<Competition[]> {
+		return await this.competitionRepository.find();
+	}
+
+	private async fetchCompetitionById(id?: number): Promise<Competition> {
+		return await this.competitionRepository.findOneOrFail({ id });
+	}
+
+	private async fetchCompetitionByName(name?: string): Promise<Competition> {
+		return await this.competitionRepository.findOneOrFail({ name });
+	}
+
+	async fetchCompetitionByIdOrName(
+		id?: number,
+		name?: string
+	): Promise<Competition> {
+		if (id) return await this.fetchCompetitionById(id);
+		return await this.fetchCompetitionByName(name);
+	}
+}
