0580db06e7f111e3d069098f314f1a23ac2fac2f
Karim <karim_daghari@outlook.com>
2020-03-17 18:03:24 +0100

    fix: import module instead of service

--- src/modules/pitch/pitch.module.ts
+++ src/modules/pitch/pitch.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PitchResolver } from './pitch.resolver';
 import { PitchService } from './pitch.service';
-import { UserService } from '../user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pitch } from './pitch.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Pitch]), UserService],
+	imports: [TypeOrmModule.forFeature([Pitch]), UserModule],
 	providers: [PitchResolver, PitchService]
 })
 export class PitchModule {}
